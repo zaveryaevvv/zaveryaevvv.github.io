f5aea896d2bb617bcabe0ade95fdc4e9ec48b231
alexey <aleksshow200410@gmail.com>
2024-08-07 17:35:26 +0300

    add planet studio files

---
+++ scripts/main_script.js
@@ -0,0 +1,9 @@
+function showDesign() {
+    document.getElementById("designSection").style.display = "block";
+    document.getElementById("websiteSection").style.display = "none";
+  }
+
+  function showWebsite() {
+    document.getElementById("designSection").style.display = "none";
+    document.getElementById("websiteSection").style.display = "block";
+  }
